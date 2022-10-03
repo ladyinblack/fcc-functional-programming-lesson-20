ca1e96b0646f9f6d6f30ac6fe3f847716208d8da
ladyinblack <ladyinblack2010rs@gmail.com>
2022-10-03 10:39:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fcc-functional-programming-lesson-20
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-n7v1qr)
---
+++ index.js
@@ -0,0 +1,28 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Combine an Array into a String Using the join Method</h1>`;
+
+/** TODO:
+ * Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str.  The function should return a string.  For example, I-like-Star-Wars would be converted to I like Star Wars.  For this challenge, do not use the replace method.
+ * 
+ function sentensify(str) {
+   // Only change code below this line 
+ 
+   // Only change code above this line
+ }
+ 
+ sentensify("May-the-force-be-with-you");
+ */
+
+function sentensify(str) {
+  // Only change code below this line
+  return str.split(/\W/).join(' ');
+  // Only change code above this line
+}
+
+console.log(sentensify('May-the-force-be-with-you'));
+console.log(sentensify('The.force.is.strong.with.this.one'));
+console.log(sentensify('There,has,been,an.awakening'));
